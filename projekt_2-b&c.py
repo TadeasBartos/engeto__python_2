0d5646210dcb5261a3d98b030e0453392af7e2d6
TadeasBoomer <100435142+TadeasBoomer@users.noreply.github.com>
2024-12-17 23:47:27 +0100

    Add files via upload

--- projekt_2-b&c.py
+++ projekt_2-b&c.py
@@ -11,19 +11,30 @@ github: TadeasBoomer
 import random
 
 def has_duplicates(num):
-    """doplnit docstringy"""
+    """Zkontroluje, zda číslo obsahuje unikátní číslice."""
     num_str = str(num)
     return len(num_str) != len(set(num_str))
 
 def carky(delka):
+    """Tiskne čárky o zadané délce."""
     print("-" * delka)
 
 def num_check(num):
+    """Vyzývá uživatele k zadání čísla do té doby, 
+        - dokud nezadá číslo o délce 4 číslic, 
+        - nezačínající 0, 
+        - volá funkci has_duplicates(num), která kontroluje zda číslo obsahuje duplicitní číslice."""
     while len(num) != 4 or num.startswith("0") or has_duplicates(num):
         num = input("Please, enter a number made of 4 digits, no letters, no duplicites and not starting with 0: ")
     return num
 
 def bulls(cislo_hrac, cislo_stroj):
+    """BULLS
+    Jedna polovina hry - vstup je číslo hráče a číslo stroje.
+    1. Vynuluje počítadlo bulls_counter
+    2. Porovnává pomocí for cyklu číslice hráče a číslice stroje, pokud jsou na stejné pozici, tak přidá do bulls_counter +1
+    3. Vrátí hodnotu int bulls counter
+    """
     bulls_counter = 0
     for i in range(0, 4):
         if cislo_hrac[i] == cislo_stroj[i]:
@@ -31,6 +42,12 @@ def bulls(cislo_hrac, cislo_stroj):
     return bulls_counter
 
 def cows(cislo_hrac, cislo_stroj):
+    """COWS
+    Druhá polovina hry - vstup je číslo hráče a číslo stroje.
+    1. Vynuluje cows counter
+    2. Porovnává pomocí for cyklu číslice hráče a číslice stroje, pokud jsou zde společné číslice, tak přidá do cows counter +1
+    3. Vrátí hodnotu int cows counter 
+    """
     cows_counter = 0 
     for j in range(0,4):
         if cislo_hrac[j] in cislo_stroj:
@@ -38,42 +55,61 @@ def cows(cislo_hrac, cislo_stroj):
     return cows_counter
 
 def print_score(bulls_counter, cows_counter):
+    """Pomocí f-stringu vrací, kolik hráč získal cows a kolik bulls. (v závorce nápověda)"""
     print(f"You have got {bulls_counter} bull{'s' if bulls_counter != 1 else ''}. (správné číslo i umístění)")
     print(f"You have got {cows_counter} cow{'s' if cows_counter != 1 else ''}. (správné číslo)")
         
-cislo_stroj = random.randint(1000,9999)
-
-while has_duplicates(str(cislo_stroj)):
-    cislo_stroj = random.randint(1000,9999)
+def bulls_and_cows():
+    """
+    Tělo hry BULLS & COWS
     
-print("Hi there!")
-carky(50)
-print("I've generated a random 4 digit number for you.")
-print("Let's play a bulls and cows game.")
-carky(50)
-cislo_hrac = num_check(input("Enter a number:  "))
-carky(50)
-print("Game started!")
-carky(50)
+    1. Vygeneruje náhodně číslo stroje 1000-9999
+    2. Následně toto generování opakuje do doby, než nesplní parametry funkce has_duplicates(num)
+    3. Uvítání hráče, zkontroluje zda zadané číslo hráče splňuje podmínky num_check(num)
+    4. Převede obě čísla z int -> str, vynuluje bulls/cows_counter a počítadlo počtu pokusů
+    5. Podmínka chodu hry je postavena tak, že cyklus while běží do doby než se číslo hráče nerovná číslu stroje
+        5.1 Proběhne funkce bulls_counter, následně cows_counter
+        5.2 Hodnoty bulls/cows_counteru jsou sděleny hráči
+        5.3 Uživatel je vyzván k zadání dalšího čísla, kontrola čísla přes funkci num_check(num)
+        5.4 Na konci while cyklu je podmínka, že pokud cislo_hrac == cislo_stroj, je uživateli sděleno že vyhrál a počet pokusů
+    """
 
-cislo_stroj = str(cislo_stroj)
-cislo_hrac = str(cislo_hrac)
+    cislo_stroj = random.randint(1000,9999)
 
-cows_counter = 0
-bulls_counter = 0
-run = 0
-
-while cislo_hrac != cislo_stroj:
-    bulls_counter = bulls(cislo_hrac=cislo_hrac, cislo_stroj=cislo_stroj)
-    cows_counter = cows(cislo_hrac=cislo_hrac, cislo_stroj=cislo_stroj)
-
-    run = run + 1
-    print("This is your ", run, " run.")
-    print_score(bulls_counter, cows_counter)
-
+    while has_duplicates(str(cislo_stroj)):
+        cislo_stroj = random.randint(1000,9999)
+        
+    print("Hi there!")
     carky(50)
-    cislo_hrac = num_check(input("Enter a number: "))
+    print("I've generated a random 4 digit number for you.")
+    print("Let's play a bulls and cows game.")
+    carky(50)
+    cislo_hrac = num_check(input("Enter a number:  "))
+    carky(50)
+    print("Game started!")
     carky(50)
 
-    if cislo_hrac == cislo_stroj:
-        print("Congratulations! You did that in", run," runs!")
+    cislo_stroj = str(cislo_stroj)
+    cislo_hrac = str(cislo_hrac)
+
+    cows_counter = 0
+    bulls_counter = 0
+    run = 0
+
+    while cislo_hrac != cislo_stroj:
+        bulls_counter = bulls(cislo_hrac=cislo_hrac, cislo_stroj=cislo_stroj)
+        cows_counter = cows(cislo_hrac=cislo_hrac, cislo_stroj=cislo_stroj)
+
+        run = run + 1
+        print("This is your ", run, " run.")
+        print_score(bulls_counter, cows_counter)
+
+        carky(50)
+        cislo_hrac = num_check(input("Enter a number: "))
+        carky(50)
+
+        if cislo_hrac == cislo_stroj:
+            print("Congratulations! You did that in", run," runs!")
+
+if __name__ == "__main__":
+     bulls_and_cows()
--- projekt_2-ttt.py
+++ projekt_2-ttt.py
@@ -8,12 +8,18 @@ github: TadeasBoomer
 import random
 
 def mrizka(herni_pole):
+    """ 
+    Funkce mrizka(herni_pole) vrací uživateli herní pole, které jako výchozí obsahuje popis polí
+    V průběhu hry mění popis pole na symbol X nebo O, dle obsazenosti pole
+    """
     max_delka = max(len(s) for s in herni_pole)
 
     def horizontal_line(max):
+        """ Funkce vypisuje symbol - dle dynamické šířky všech polí"""
         return f"+{'-' * (2 + max)}+{'-' * (2 + max)}+{'-' * (2 + max)}+"
 
     def format_row(start):
+        """ Funkce vypisuje herní pole. Jako výchozí hodnota je tam popis pole, ta je následně vyměněna za symbol X/O, podle volby hráče"""
         return f"| {herni_pole[start]:^{max_delka}} | {herni_pole[start+1]:^{max_delka}} | {herni_pole[start+2]:^{max_delka}} |"
 
     print(horizontal_line(max_delka))
@@ -24,51 +30,70 @@ def mrizka(herni_pole):
     print(format_row(6))
     print(horizontal_line(max_delka))
 
-print("Vítejte ve hře piškvorky!")
-print("Tvým úkolem je vytvořit řadu tří symbolů X na hrací ploše.")
-print("Hrací ploše vypadá následovně: ")
+def piskvorky():
+    """
+    Tělo hry PIŠKVORKY
 
-herni_pole = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
-mrizka(herni_pole)
+    1. Hráč je uveden do hry a je mu ukázáno výchozí herní pole
+    2. Do proměnné volne_pole je vytvořena kopie herni_pole. vyherni_pole je list 8 kombinací možné výhry, kdy nezáleží na pořadí tahů
+    3. Jsou vytvořeny prázdné listy tahů tahy_hrac[]/tahy_pocitac[]
+    4. Cyklus hry je tvořen přes for cyklus o pěti krocích. 
+        4.1 Hráče je vyzván na tah. Následně je zkontrolováno, zda se tah_hrac nenachází ve volne_pole.
+        4.2 Do listu tahy_hrac[] se přidá element tah_hrac, toto pole je odebráno z listu volne_pole a do funkce mrizka() je mu přirazen symbol "X".
+        4.3 Je zkontrolováno zda tento tah nebyl výherní.
+        4.4 Pokud list volne_pole stále nějaká pole obsahuje, je tah_pocitace proveden jako náhodná volba z prvků listu volne_pole. Následují stejné kroky jako u hráče - přidání do listu tahy_pocitace[], odebrání z listu volne_pole[] a přiřazení symbolu O.
+        4.5 Je zkontrolování, zda tah_pocitac nebyl výherní.
+        4.6 Pokud je list volne_pole prázdný, je vyhlášena remíza.
+        4.7 Hráči je ukázano aktualizované herní pole - s novými tahy.    
+    """
+    print("Vítejte ve hře piškvorky!")
+    print("Tvým úkolem je vytvořit řadu tří symbolů X na hrací ploše.")
+    print("Hrací ploše vypadá následovně: ")
 
-print("Vždy dostaneš na výběr, na jaké pole chceš svůj kámen umístit.")
-print("Pokud vybereš obsazené pole nebo něco jiného, budeš vyzván znovu, dokud nevybereš pole volné.")
+    herni_pole = ["a1", "a2", "a3", "b1", "b2", "b3", "c1", "c2", "c3"]
+    mrizka(herni_pole)
 
-volne_pole = herni_pole.copy()
-vyherni_pole = [("a1", "a2", "a3"), ("b1", "b2", "b3"), ("c1", "c2", "c3"),
-                ("a1", "b1", "c1"), ("a2", "b2", "c2"), ("a3", "b3", "c3"),
-                ("a1", "b2", "c3"), ("a3", "b2", "c1")]
+    print("Vždy dostaneš na výběr, na jaké pole chceš svůj kámen umístit.")
+    print("Pokud vybereš obsazené pole nebo něco jiného, budeš vyzván znovu, dokud nevybereš pole volné.")
 
-tahy_hrac = []
-tahy_pocitac = []
+    volne_pole = herni_pole.copy()
+    vyherni_pole = [("a1", "a2", "a3"), ("b1", "b2", "b3"), ("c1", "c2", "c3"),
+                    ("a1", "b1", "c1"), ("a2", "b2", "c2"), ("a3", "b3", "c3"),
+                    ("a1", "b2", "c3"), ("a3", "b2", "c1")]
 
-for _ in range(5):
-    tah_hrac = input("Zadej jaké chceš hrát volné pole: ")
-    while tah_hrac not in volne_pole:
-        tah_hrac = input("Pole je obsazené nebo neexistuje. Zadej jiné pole: ")
-    
-    tahy_hrac.append(tah_hrac)
-    volne_pole.remove(tah_hrac)
-    herni_pole[herni_pole.index(tah_hrac)] = "X"
+    tahy_hrac = []
+    tahy_pocitac = []
 
-    if any(set(pole).issubset(tahy_hrac) for pole in vyherni_pole):
-        print("Vyhráváš ty!")
-        mrizka(herni_pole)
-        break
+    for _ in range(5):
+        tah_hrac = input("Zadej jaké chceš hrát volné pole: ")
+        while tah_hrac not in volne_pole:
+            tah_hrac = input("Pole je obsazené nebo neexistuje. Zadej jiné pole: ")
+        
+        tahy_hrac.append(tah_hrac)
+        volne_pole.remove(tah_hrac)
+        herni_pole[herni_pole.index(tah_hrac)] = "X"
 
-    if volne_pole:
-        tah_pocitac = random.choice(volne_pole)
-        tahy_pocitac.append(tah_pocitac)
-        volne_pole.remove(tah_pocitac)
-        herni_pole[herni_pole.index(tah_pocitac)] = "O"
-
-        if any(set(pole).issubset(tahy_pocitac) for pole in vyherni_pole):
-            print("Vyhrává počítač!")
+        if any(set(pole).issubset(tahy_hrac) for pole in vyherni_pole):
+            print("Vyhráváš ty!")
             mrizka(herni_pole)
             break
 
-    if not volne_pole:
-        print("Je to remíza!")
-        break
+        if volne_pole:
+            tah_pocitac = random.choice(volne_pole)
+            tahy_pocitac.append(tah_pocitac)
+            volne_pole.remove(tah_pocitac)
+            herni_pole[herni_pole.index(tah_pocitac)] = "O"
 
-    mrizka(herni_pole)
+            if any(set(pole).issubset(tahy_pocitac) for pole in vyherni_pole):
+                print("Vyhrává počítač!")
+                mrizka(herni_pole)
+                break
+
+        if not volne_pole:
+            print("Je to remíza!")
+            break
+
+        mrizka(herni_pole)
+
+if __name__ == __name__:
+    piskvorky()
